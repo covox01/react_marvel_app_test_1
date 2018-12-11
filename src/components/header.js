f8d5b6b7e7c98017618acc2c054275684dd5b43f
Dennis Oba <oba.dennis@gmail.com>
2018-12-11 18:30:04 -0500

    Merge Upstream to Master

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FilterBar from './FilterBar';
+import './Header.scss';
+
+function Header(props) {
+	return (
+		<header className="Header__Component">
+			<h1>{props.title}</h1>
+			<p>{props.message}</p>
+			<FilterBar />
+		</header>
+	);
+};
+
+export default Header;
