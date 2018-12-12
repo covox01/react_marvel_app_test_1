e4b97a6320d2fad6c4a83f1fb9abe1aad23d0a39
Dennis Oba <oba.dennis@gmail.com>
2018-12-12 00:24:15 -0500

    Kind of added a a handle for filter bar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import './App.scss';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import FilterBar from './components/FilterBar';
+import './App.scss';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  onFilterSubmit = (searchQuery) => {
+    console.log("In app.js" + searchQuery);
+  }
+
   render() {
     return (
       <div className="App__Component">
         <Header 
           title="Marvel Comic Explorer"
           message="Search and Explore your Favourite Marvel Characters"
+          onFilterSubmit={this.onFilterSubmit}
         />
         <Footer />
       </div>
     );
   }
 }
+
+ 
 
 export default App;
--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import './FilterBar.scss';
 
 class FilterBar extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			searchQuery: ""
+		}
+	}
+
+	handleSearchChange = (event) => {
+		console.log(event.target.value)
+		this.setState({
+			searchQuery: event.target.value
+		});
+	}
+
+	handleFormSubmit = (event) => {
+		event.preventDefault();
+
+		this.props.onFilterSubmit(this.state.inputValue);
+
+		console.log(this.state.inputValue);
+
+	}
+
 	render() {
 		return (
 			<div className="FilterBar__Component">
@@ -9,7 +32,11 @@ class FilterBar extends Component {
 	              <label className="visually-hidden" htmlFor="search">
 	                Search Characters:
 	              </label>
-	              <input type="text" placeholder="Search characters" id="search"/>
+	              <input type="text" 
+	              		 placeholder="Search characters" 
+	              		 id="search" 
+	              		 searchQuery={this.state.searchQuery} 
+	              		 onChange={this.handleSearchChange}/>
 	              <button type="submit">Submit</button>
 	            </form>
 	        </div>
